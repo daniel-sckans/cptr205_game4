2070b26adfcb70b604f6a94efa0c811136511a5e
daniel-sckans <evan.daniel@sckans.edu>
2021-03-08 14:32:52 -0600

    platformer assignment

--- main.js
+++ main.js
@@ -2,60 +2,106 @@ window.addEventListener('DOMContentLoaded', DOMContentLoaded => {
     
     // CANVAS INIT
     const render = document.querySelector('canvas').getContext('2d'); 
+    let w, h; 
     const resize = () => {
-        render.canvas.width = render.canvas.clientWidth * window.devicePixelRatio; 
-        render.canvas.height = render.canvas.clientHeight * window.devicePixelRatio; 
+        w = render.canvas.width = render.canvas.clientWidth * window.devicePixelRatio; 
+        h = render.canvas.height = render.canvas.clientHeight * window.devicePixelRatio; 
     }; 
     resize(); 
     window.addEventListener('resize', resize); 
 
     // APPLY PHYSICS
-    let player_x = -1, player_y = 0; 
+    let player_x = 0, player_y = 0; 
     let player_r = 0.2; 
     let player_vx = 0, player_vy = 0; 
     let player_ax = 0, player_ay = 0; 
+    let arrow_right = false; 
+    let arrow_left = false; 
     document.addEventListener('keydown', keydown => {
         if(keydown.key === 'ArrowRight') {
-            player_ax += 0.01; 
+            arrow_right = true; 
         }
         if(keydown.key === 'ArrowLeft') {
-            player_ax -= 0.01; 
+            arrow_left = true; 
         }
         if(keydown.key === 'ArrowUp') {
             player_ay += 0.01; 
         }
     }); 
+    document.addEventListener('keyup', keyup => {
+        if(keyup.key === 'ArrowRight') {
+            arrow_right = false; 
+        }
+        if(keyup.key === 'ArrowLeft') {
+            arrow_left = false; 
+        }
+    }); 
+
+    // PLATFORM
+    class Platform {
+        constructor(x, y, w, h) {
+            this.x = x; 
+            this.y = y; 
+            this.w = w; 
+            this.h = h; 
+        }
+    }
+    const platforms = []; 
+    platforms.push(new Platform(-0.25, -0.25, 3, 0.25)); 
+    platforms.push(new Platform(0.125, 0.25, 4, 0.125)); 
     
     // ANIMATION LOOP
     const animation = timestamp => {
-        render.clearRect(0, 0, render.canvas.width, render.canvas.height); 
-
-        render.fillStyle = '#0f0'; 
-        render.fillRect(0, render.canvas.height / 2, render.canvas.width, render.canvas.height / 2); 
-
-        render.fillStyle = '#0ff'; 
-        render.fillRect(0, 0, render.canvas.width, render.canvas.height / 2); 
-
-        render.fillStyle = '#f00'; 
-        render.beginPath(); 
-        const w = render.canvas.width, h = render.canvas.height; 
-        
+        const PLAYER_ACCELERATION_X = 0.001; 
+        if(arrow_right) {
+            player_ax += PLAYER_ACCELERATION_X; 
+        }
+        if(arrow_left) {
+            player_ax -= PLAYER_ACCELERATION_X; 
+        }
         player_vx += player_ax; 
         player_x += player_vx; 
         player_ax = 0; 
         player_vx *= 0.98; 
         player_vy += player_ay; 
-        player_y += player_vy; 
-        if(0 < player_y) {
-            player_ay -= 0.00098; 
-        } else {
-            player_ay = 0; 
-            player_vy = 0; 
-            player_y = 0; 
+        player_ay -= 0.00098; 
+        
+        let player_grounded = false; 
+        platforms.forEach(platform => {
+            if(platform.x <= player_x && player_x <= platform.x + platform.w && platform.y <= player_y && player_y + player_vy <= platform.y) {
+                player_grounded = true; 
+                player_ay = 0; 
+                player_vy = 0; 
+                player_y = platform.y; 
+                return; 
+            }
+        }); 
+        if(!player_grounded) {
+            player_y += player_vy; 
         }
-        render.arc(player_x * w / 2 + w / 2, -player_y * h / 2 + h / 2, player_r * w / 2, 0, 2 * Math.PI); 
+    
+        render.save(); 
+        render.translate(-player_x * w / 2, player_y * h / 2); 
+        
+        render.clearRect(player_x * w / 2, -player_y * h / 2, render.canvas.width, render.canvas.height); 
+
+        render.fillStyle = '#0f0'; 
+        render.fillRect(-1000, render.canvas.height / 2, 10000 * render.canvas.width, render.canvas.height / 2); 
+
+        render.fillStyle = '#0ff'; 
+        render.fillRect(-1000 * w, 0, 10000 * render.canvas.width, render.canvas.height / 2); 
+        
+        render.fillStyle = '#00f'; 
+        platforms.forEach(platform => {
+            render.fillRect(platform.x * w / 2 + w / 2, -platform.y * h / 2 + h / 2, platform.w * w / 2, platform.h * h / 2); 
+        }); 
+        
+        render.fillStyle = '#f00'; 
+        render.beginPath(); 
+        render.arc(player_x * w / 2 + w / 2, -player_y * h / 2 + h / 2 - player_r * h / 2, player_r * h / 2, 0, 2 * Math.PI); 
         render.fill(); 
         
+        render.restore(); 
         window.requestAnimationFrame(animation); 
     }; 
     window.requestAnimationFrame(animation); 
